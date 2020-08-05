5c77476425acc94e8d3232dd1c2ee92a7832c124
Gloria Zheng <G.Zheng@ampleforth.org.uk>
2020-08-05 19:47:34 +0100

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,36 @@
+﻿
+Module Module1
+
+    Sub Main()
+
+        Dim mark As String
+
+        Console.WriteLine("Please enter your mark: ")
+
+        mark = Console.ReadLine
+
+        Convert.ToInt16(mark)
+
+        If mark >= 80 Then
+
+            Console.WriteLine("Distinction!")
+
+        ElseIf mark >= 60 Then
+
+            Console.WriteLine("Merit")
+
+        ElseIf mark >= 40 Then
+
+            Console.WriteLine("Pass")
+
+        Else
+
+            Console.WriteLine("Fail")
+
+        End If
+
+        Console.Read()
+
+    End Sub
+
+End Module
